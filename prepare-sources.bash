03579c0bdd54306dcc532d5906af529e4642ebdb
Christian Velin <christian@dotslashme.com>
2022-10-19 14:13:36 +0200

    Added parameter for path

--- prepare-sources.bash
+++ prepare-sources.bash
@@ -5,7 +5,7 @@ declare source_dir
 declare latest_version
 script_path=$(dirname "$0")
 script_path=$(cd "${script_path}" && pwd)
-source_dir="${script_path}/../activemq_artemis"
+source_dir="$1"
 readonly source_dir
 
 # Clone Artemis source code repository
@@ -14,7 +14,7 @@ git clone https://github.com/apache/activemq-artemis.git "${source_dir}"
 # Build either a provided version or latest tag from the repository
 cd "${source_dir}" || exit 1
 latest_version="$(git describe --tags --abbrev=0)"
-artemis_version="${1:-$latest_version}"
+artemis_version="${2:-$latest_version}"
 readonly artemis_version
 
 # Echo version used back to caller
